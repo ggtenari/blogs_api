d6887933f7dd6fc4012aa9c946af596817b1cedd
Gabriel Tenari <gabriel.tenari.ismart@gmail.com>
2022-10-26 15:36:30 -0300

    feat: postCategory model created

---
+++ src/models/PostCategory.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory',
+  {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    }
+  },
+  {
+    timestamps: false,
+    underscored: true, 
+    tableName: 'posts_categories'
+  })
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'post_id',
+      otherKey: 'category_id'
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'category_id',
+      otherKey: 'post_id'
+    })
+  }
+
+  return PostCategory;
+}
